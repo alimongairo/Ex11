10b720c6ed3a347b14aed35892b4346042df4160
Irina <pasmanikirina29@gmail.com>
2021-04-05 13:26:13 +0300

    file addition

---
+++ include/train.h
@@ -0,0 +1,22 @@
+#pragma once
+// Copyright 2021 by idpas
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
+
+class Cage {
+  bool light;  // Свет (вкл/выкл)
+ public:
+  Cage* next;  // следующий вагон
+  Cage* prev;  // предыдущий вагон
+  Cage() : light(false), next(nullptr), prev(nullptr) {}
+  void on() { light = true; }
+  void off() { light = false; }
+  bool get() const { return light; }
+};
+
+class Train {
+  Cage* first;  // указатель на первый вагон
+  Cage* last;   // указатель на последний вагон
+};
+
+#endif  // INCLUDE_TRAIN_H_
--- src/train.cpp
+++ src/train.cpp
@@ -2,7 +2,4 @@
 #include <iostream>
 #include "train.h"
 
-int main() {
-  // TODO
-	return 0;
-}
+//TODO
