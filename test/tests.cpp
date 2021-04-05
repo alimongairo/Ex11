16b2e41748ca4fea464878b4f705cc13c4e87fad
Irina <pasmanikirina29@gmail.com>
2021-04-05 22:57:50 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,5 +3,33 @@
 #include <string>
 #include "train.h"
 
+TEST(Train, test_1) {
+  Train tr;
+  size_t expected = 0;
+  size_t result = train.get_length();
+  EXPECT_EQ(expected, result);
+}
 
+TEST(Train, test_2) {
+  Train tr;
+  for (int i = 0; i < 10; i++) {
+    Cage* cg = new Cage();
+    tr.add_cage(cg);
+  }
+  size_t expected = 10;
+  size_t result = train.get_length();
+  EXPECT_EQ(expected, result);
+}
 
+TEST(Train, test_2) {
+  Train tr;
+  for (int i = 0; i < 10; i++) {
+    Cage* cg = new Cage();
+    tr.add_cage(cg);
+  }
+  Cage* cg_2 = new Cage();
+  tr.add_cage(cg_2);
+  size_t expected = 11;
+  size_t result = train.get_length();
+  EXPECT_EQ(expected, result);
+}
